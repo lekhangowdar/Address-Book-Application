1dbb0040482d76c3b2024c3dcb80780f717e0d81
Lekhan Gowda R <143644772+lekhangowdar@users.noreply.github.com>
2025-04-23 08:10:00 +0530

    Create populate.h

---
+++ populate.h
@@ -0,0 +1,17 @@
+/***************************************************************
+ * Name        : Lekhan Goeda R
+ * Project     : Address Book
+ * File Name   : populate.h
+ * Description : Declares the function used to populate the address
+ *               book with default or initial data if needed.
+ ***************************************************************/
+
+#ifndef POPULATE_H
+#define POPULATE_H
+
+#include "contact.h"
+
+// Function to populate the address book with initial contacts
+void populateAddressBook(AddressBook* addressBook);
+
+#endif
